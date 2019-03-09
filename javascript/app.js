30fee62e1cfd83355e2013e521928dc4657f82e9
jzubak <jzubak81@gmail.com>
2019-03-09 15:59:58 -0500

    SATURDAY 4pm

--- javascript/app.js
+++ javascript/app.js
@@ -1,13 +1,48 @@
-var questions = {
-    question1: {
+var questionTimeout = setInterval(showQuestion,5*1000);
+var questions = [
+        {
         question: "Test Question",
         answers: ["a", "b", "c", "d"],
         correctAnswer: "b",
+        },
+    {
+    question: "Test Question2",
+    answers: ["a", "b", "c", "d"],
+    correctAnswer: "c",
+    },
+    {question: "Test Question3",
+    answers: ["a", "b", "c", "d"],
+    correctAnswer: "c",
+    },
+    {question: "Test Question4",
+    answers: ["a", "b", "c", "d"],
+    correctAnswer: "c",
     }
+]
+var count = 0;
+var start = $("<button>");
+$(start).text("Start");
+$(".questionBox").prepend(start);
+
+
+function showQuestion() {
+    
+            $(".questionBox").html("<div>" + questions[count].question + " </div>");
+    for (var i = 0; i < questions[count].answers.length; i++){
+            
+           $(`.answer${i+1}`).text(questions[count].answers[i]);
+                   
+    }
+    count++;
+    // (questionTimeout)
 }
 
-var questionTimeout = setTimeout(function() {
-    alert("Alert #1: Called automatically 1 second after page load.");
-}, 5*1000);
+$(start).on("click", function (startGame) {
 
-  (questionTimeout)
+    showQuestion()
+    // $(questionTimeout) = setInterval(nextQuestion, 5*1000);
+
+});
+
+///store answer selected for later total 
+//
